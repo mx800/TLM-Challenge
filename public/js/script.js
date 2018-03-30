25196f9c3d864f49d82475189dacac3b59eec370
mx800 <mxmx800@hotmail.com>
2018-03-30 13:08:06 -0400

    Async call for mongodb

--- public/js/script.js
+++ public/js/script.js
@@ -8,17 +8,22 @@ $('.vider').on('click', function() {
 
 });
 
-/*
-//Pour passer a la page ninjify
-$('.continuer').on('click', function() {
-    location.href = "/ninjify"
-});
-*/
 
+//Pour passer a la page ninjify avec paramètre
+$('.continuer').on('click', function() {
+    let data=($('#gender-select').dropdown('get value'));
+    let param='';
+    for(let i=0; i< data.length-1;i++){
+        param += data[i]+',';
+    }
+    location.href = `/ninjify?x=${param}`;
+});
+
+/*
 $('.item').on('click',function(){
     path = window.location.pathname;
 })
-
+*/
 
 
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const async = require('async');
 
 
 //-------------------------------------
@@ -49,8 +50,8 @@ app.set('view engine','ejs')
 //Middleware
 //-------------------------------------
 app.use('/asset', express.static('public'))
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({extended:false}))
+//app.use(bodyParser.json())
 
 
 //----------------------------------
@@ -59,22 +60,42 @@ app.use(bodyParser.json())
 app.get('/', (request, response)=>{
     response.render('pages/home')
 })
+
 app.get('/jeux', (request, response)=>{
     //Récupère le data
     BuzzwordGroup.find({},function(err,buzzwordGroups){
         if(err){
             console.log(err)
-           // response.send({error:err});
         }
         response.render('pages/index',{buzzwordGroups: buzzwordGroups})
     });
 })
-//app.get('/ninjify', (request, response)=>{
-//    response.render('pages/ninjify')
-//})
+
 app.get('/ninjify/', (request, response)=>{
-    response.render('pages/ninjify')
+   let param = request.query.x.split(',');
+   let buzzwordCollection=[];
+   let calls =[];
+
+       for(let i=0;i<param.length-1;i++) {
+            calls.push(function (callback) {
+                BuzzwordGroup.find({buzzword: param[i]}, (err, name) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    callback(null, name)
+                })
+            })
+        }
+        //Fonction asynchrone du Foreach
+        async.parallel(calls, function(err,result){
+            if (err) {
+                console.log(err)
+            }
+            console.log(result)
+            response.render('pages/ninjify',{buzzwords: result})
+        })
 })
+
 app.get('/*', (request, response)=>{
     response.redirect('/')
 })
